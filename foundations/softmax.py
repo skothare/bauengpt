e4c9bdf5268b944113b33a0d866d365cbba08d82
Sumeet Kothare <21262952+skothare@users.noreply.github.com>
2026-06-21 15:51:16 -0400

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -8,8 +8,21 @@ class Solution:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
         # return np.round(your_answer, 4)
-        z_maxsubtract = z - max(z)
-        exp_z = np.exp(z_maxsubtract)
-        softmax = exp_z / np.sum(exp_z)
+
+        #METHOD 1 (potentially, max(z) is inefficient since it unpacks the entire vector)
+        # z_maxsubtract = z - max(z)
+        # exp_z = np.exp(z_maxsubtract)
+        # softmax = exp_z / np.sum(exp_z)
+
+        
+
+        # METHOD 2:
+        # 1. Make this function z-dimension agnostic
+        # 2. Runtime efficient
+        
+        # Sum over the last dimensional axis and keep the structural shape the same
+        z_maxsubstract = z - np.max(z, axis=-1, keepdims=True)
+        exp_z = np.exp(z_maxsubstract)
+        softmax = exp_z / np.sum(exp_z, axis=-1, keepdims=True)
 
         return np.round(softmax, 4)
